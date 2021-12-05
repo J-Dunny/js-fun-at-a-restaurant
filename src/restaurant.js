4795c3da283c1a2a75d04eb3bc49140baabf1d5c
Jake Dunafon <jlad795@gmail.com>
2021-12-04 20:32:30 -0700

    Streamline removeMenuItem

--- src/restaurant.js
+++ src/restaurant.js
@@ -27,32 +27,32 @@ function addMenuItem(restaurant, item) {
 
 
 function removeMenuItem(restaurant, name, type){
-  if(type = "breakfast"){
-    for(var i = 0; i < restaurant.menus.breakfast.length; i ++){
-      if(restaurant.menus.breakfast[i].name === name){
-        restaurant.menus.breakfast.splice(i, 1);
+  // if(type = "breakfast"){
+    for(var i = 0; i < restaurant.menus[type].length; i ++){
+      if(restaurant.menus[type][i].name === name){
+        restaurant.menus[type].splice(i, 1);
 
         return `No one is eating our ${name} - it has been removed from the ${type} menu!`
       }
     }
-  }
-  if(type = "lunch"){
-    for(var i = 0; i < restaurant.menus.lunch.length; i ++){
-      if(restaurant.menus.lunch[i].name === name){
-        restaurant.menus.lunch.splice(i, 1);
-
-        return `No one is eating our ${name} - it has been removed from the ${type} menu!`
-      }
-    }
-  }
-  if(type = "dinner"){
-    for(var i = 0; i < restaurant.menus.dinner.length; i ++){
-      if(restaurant.menus.dinner[i].name === name){
-        restaurant.menus.dinner.splice(i, 1);
-        return `No one is eating our ${name} - it has been removed from the ${type} menu!`
-      }
-    }
-  }
+  // }
+  // if(type = "lunch"){
+  //   for(var i = 0; i < restaurant.menus.lunch.length; i ++){
+  //     if(restaurant.menus.lunch[i].name === name){
+  //       restaurant.menus.lunch.splice(i, 1);
+  //
+  //       return `No one is eating our ${name} - it has been removed from the ${type} menu!`
+  //     }
+  //   }
+  // }
+  // if(type = "dinner"){
+  //   for(var i = 0; i < restaurant.menus.dinner.length; i ++){
+  //     if(restaurant.menus.dinner[i].name === name){
+  //       restaurant.menus.dinner.splice(i, 1);
+  //       return `No one is eating our ${name} - it has been removed from the ${type} menu!`
+  //     }
+  //   }
+  // }
 
   return `Sorry, we don't sell ${name}, try adding a new recipe!`
 }
